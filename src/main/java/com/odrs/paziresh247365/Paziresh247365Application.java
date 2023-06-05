39894c1f366407a42e6d97dfd247fc2c66f1485f
ali-khosravani <ali.p3soft@gmail.com>
2023-06-05 03:46:50 +0330

    start final project -Online Doctor Reservation System

---
+++ src/main/java/com/odrs/paziresh247365/Paziresh247365Application.java
@@ -0,0 +1,13 @@
+package com.odrs.paziresh247365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paziresh247365Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Paziresh247365Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/odrs/paziresh247365/Paziresh247365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odrs.paziresh247365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paziresh247365ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
