ac3392608a4239160df5440c00de333f7423b24f
ali-khosravani <ali.p3soft@gmail.com>
2023-06-14 12:32:24 +0330

    connect to database

--- src/main/java/com/odrs/paziresh247365/controller/LoginController.java
+++ src/main/java/com/odrs/paziresh247365/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.odrs.paziresh247365.controller;
 
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,8 +9,8 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class LoginController {
     @RequestMapping("/login")
-    public ModelAndView login(){
-        ModelAndView modelAndView =new ModelAndView();
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login.html");
         return modelAndView;
     }
---
+++ src/main/java/com/odrs/paziresh247365/model/User.java
@@ -0,0 +1,72 @@
+package com.odrs.paziresh247365.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tbl_User")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return id == user.id && Objects.equals(fullName, user.fullName) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, email, password);
+    }
+}
